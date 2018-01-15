45267e02376ef7e44a6e13b391f231408857f05d
Lauren Schwartz <lschwartzwriting@gmail.com>
2018-01-15 19:38:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n){
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list');
+  for (let i = 0; i < list.length; i++){
+    list[i].innerHTML = (i + 1).toString();
+  }
+}
+
+function deepestChild(){
+  const boo = document.getElementById('grand-node').querySelectorAll('div');
+  for (let i = 0: i < boo.length; i++){
+    
+  }
+}
